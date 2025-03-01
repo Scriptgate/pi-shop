c62e8d485818c54a7b4d0de00b63ac731d81aff6
Scriptgate <wscheppers@gmail.com>
2025-03-01 23:58:01 +0100

    angular lesson 05: https://angular.dev/tutorials/first-app/05-inputs

--- frontend/src/app/housing-location/housing-location.component.ts
+++ frontend/src/app/housing-location/housing-location.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
-
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HousingLocation} from '../housinglocation';
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [CommonModule],
   template: `
     <p>
       housing-location works!
     </p>
   `,
-  styles: ``
+  styleUrls: ['./housing-location.component.css'],
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
