4957ae735f95cb67ff2961dd5dcf81356d441c81
Scriptgate <wscheppers@gmail.com>
2025-03-02 19:46:17 +0100

    angular lesson 12: https://angular.dev/tutorials/first-app/12-forms

--- frontend/src/app/housing.service.ts
+++ frontend/src/app/housing.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HousingLocation} from './housinglocation';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HousingService {
   readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
@@ -112,5 +112,10 @@ export class HousingService {
   }
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
+  }
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`,
+    );
   }
 }
