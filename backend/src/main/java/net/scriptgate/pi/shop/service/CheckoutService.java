00235cd910dd20b658e2e65caa95ee9f98cf6a6d
Scriptgate <wscheppers@gmail.com>
2025-06-06 21:49:38 +0200

    Move messaging template from BarcodeScanner to CheckoutService

--- backend/src/main/java/net/scriptgate/pi/shop/service/CheckoutService.java
+++ backend/src/main/java/net/scriptgate/pi/shop/service/CheckoutService.java
@@ -1,6 +1,8 @@
 package net.scriptgate.pi.shop.service;
 
 import net.scriptgate.pi.shop.controller.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,10 +11,13 @@ import java.util.List;
 @Service
 public class CheckoutService {
 
+    @Autowired private SimpMessagingTemplate template;
+
     private final List<Product> shoppingCart = new ArrayList<>();
 
     public void add(Product product) {
         shoppingCart.add(product);
+        template.convertAndSend("/checkout/products", getProducts());
     }
 
     public List<Product> getProducts() {
