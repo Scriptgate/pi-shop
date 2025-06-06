b53049e426982ed3594ae064b19fce08e1850d07
Scriptgate <wscheppers@gmail.com>
2025-06-06 23:13:24 +0200

    implement deleting products from checkout

--- backend/src/main/java/net/scriptgate/pi/shop/service/CheckoutService.java
+++ backend/src/main/java/net/scriptgate/pi/shop/service/CheckoutService.java
@@ -1,15 +1,20 @@
 package net.scriptgate.pi.shop.service;
 
 import net.scriptgate.pi.shop.controller.model.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CheckoutService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CheckoutService.class);
 
     @Autowired private SimpMessagingTemplate template;
 
@@ -22,5 +27,15 @@ public class CheckoutService {
 
     public List<Product> getProducts() {
         return shoppingCart;
+    }
+
+    public void remove(int id) {
+        Optional<Product> productToDelete = shoppingCart.stream().filter(product -> product.getId() == id).findFirst();
+        if (productToDelete.isPresent()) {
+            shoppingCart.remove(productToDelete.get());
+            template.convertAndSend("/checkout/products", getProducts());
+        } else {
+            LOG.warn("Could not delete product: id '{}' not found", id);
+        }
     }
 }
