89c7f572f38ec259db3e2d1e93e695124a4e30d9
Scriptgate <wscheppers@gmail.com>
2025-06-25 23:05:31 +0200

    - Rename backend to backend-for-frontend - disable spring boot for all projects except startup - Data loader wasn't picked up, add logging - Add componentscan to pick up dataloader - Add log level to print data loader info

--- startup/src/main/java/net/scriptgate/pi/shop/PiShopApplication.java
+++ startup/src/main/java/net/scriptgate/pi/shop/PiShopApplication.java
@@ -2,8 +2,10 @@ package net.scriptgate.pi.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"net.scriptgate.pi.shop"})
 public class PiShopApplication {
 
     public static void main(String[] args) {
--- startup/src/main/java/net/scriptgate/pi/shop/data/DataLoader.java
+++ startup/src/main/java/net/scriptgate/pi/shop/data/DataLoader.java
@@ -1,6 +1,9 @@
 package net.scriptgate.pi.shop.data;
 
+import net.scriptgate.pi.shop.component.BarcodeScanner;
 import net.scriptgate.pi.shop.ports.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -13,6 +16,8 @@ import java.io.InputStream;
 @Component
 public class DataLoader implements ApplicationRunner {
 
+    private static final Logger LOG = LoggerFactory.getLogger(BarcodeScanner.class);
+
     private ProductRepository productRepository;
 
     @Autowired
@@ -22,12 +27,14 @@ public class DataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
+        LOG.info("Loading data...");
         productRepository.save("Frietjes", image("images/fries.png"), "FOOD", "1001", "5.00");
         productRepository.save("Broodje", image("images/bread.png"), "FOOD", "1002", "3.00");
         productRepository.save("Koffie", image("images/coffee.png"), "DRINK", "1003", "12.00");
         productRepository.save("Appelsiensap", image("images/juice.png"), "DRINK", "1004", "4.00");
         productRepository.save("Donuts", image("images/donuts.png"), "FOOD", "1005", "18.00");
         productRepository.save("Koekjes", image("images/cookies.png"), "FOOD", "1006", "25.00");
+        LOG.info("Data loaded.");
     }
 
     private byte[] image(String path) {
